0c796f0f2269941823d76839848c06b51b0a8cc6
Leondre Bennett Souza de Oliveira <2023105070003@aluno.ifro.edu.br>
2025-07-03 11:32:01 -0400

    começo

---
+++ vue-project-version-1/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue';
+</script>
+
+<template>
+ 
+  <main>
+    <HelloWorld />
+  </main>
+      
+</template>
+
+<style scoped>
+
+</style>
+
---
+++ vue-project-version-1/src/components/HelloWorld.vue
@@ -0,0 +1,109 @@
+<script lang="ts">
+  export default {
+    data: () => ({
+      tab: null,
+    }),
+  }
+  const ex4 = ['red', 'indigo', 'orange', 'primary', 'secondary', 'success', 'info', 'warning', 'error', 'red darken-3', 'indigo darken-3', 'orange darken-3']
+</script>
+
+<template>
+  <main>
+    <v-card
+      class="card"
+      title="Card title"
+      subtitle="Subtitle"
+      text="hello world"
+      variant= "outlined"
+    ></v-card>
+    <v-btn>
+  botao basico
+</v-btn>
+<v-icon color="error" icon="mdi-alert" size="x-large"></v-icon>
+  <v-card>
+    <v-tabs
+      v-model="tab"
+      bg-color="primary"
+    >
+      <v-tab value="one">Item One</v-tab>
+      <v-tab value="two">Item Two</v-tab>
+      <v-tab value="three">Item Three</v-tab>
+    </v-tabs>
+
+    <v-card-text>
+      <v-tabs-window v-model="tab">
+        <v-tabs-window-item value="one">
+          One
+        </v-tabs-window-item>
+
+        <v-tabs-window-item value="two">
+          Two
+        </v-tabs-window-item>
+
+        <v-tabs-window-item value="three">
+          Three
+        </v-tabs-window-item>
+      </v-tabs-window>
+    </v-card-text>
+  </v-card>
+  <v-carousel show-arrows="hover">
+  <v-carousel-item
+    src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+    cover
+  ></v-carousel-item>
+
+  <v-carousel-item
+    src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg"
+    cover
+  ></v-carousel-item>
+
+  <v-carousel-item
+    src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+    cover
+  ></v-carousel-item>
+</v-carousel>
+
+<v-dialog max-width="500">
+  <template v-slot:activator="{ props: activatorProps }">
+    <v-btn
+      v-bind="activatorProps"
+      color="surface-variant"
+      text="Open Dialog"
+      variant="flat"
+    ></v-btn>
+  </template>
+
+  <template v-slot:default="{ isActive }">
+    <v-card title="Dialog">
+      <v-card-text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+
+        <v-btn
+          text="Close Dialog"
+          @click="isActive.value = false"
+        ></v-btn>
+      </v-card-actions>
+    </v-card>
+  </template>
+</v-dialog>
+<v-toolbar density="compact" :elevation="8" title="lerolero"></v-toolbar>
+
+
+  </main>
+</template>
+
+<style scoped>
+main {
+  width: 100%;
+  margin: 0%;
+  padding: 0%;
+}
+
+.card {
+  width: 500px;
+}
+</style>
